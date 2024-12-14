05ac1b045f2210d908ff5a7822315175b72644d1
kabirasunny <sunnykabira5507@gmail.com>
2024-12-14 14:50:42 +0530

    I wrote js for changeBtn for change color for background body

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 
+
 document.getElementById('greenBtn').addEventListener("click", function () {
     fetch('/api/greet') //Fetch the greeting from the API
         .then(response => response.json())
@@ -8,5 +9,19 @@ document.getElementById('greenBtn').addEventListener("click", function () {
         })
         .catch(error => {
             console.error('Error fetching greeting:', error)
+        })
+})
+
+let body = document.querySelector('body');
+
+document.getElementById('changeBtn'), addEventListener("click", () => {
+    fetch('/api/color')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.color)
+            body.style.background = data.color;
+        })
+        .catch(error => {
+            console.error('Error fetching color:', error)
         })
 })
