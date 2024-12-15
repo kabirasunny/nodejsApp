3e80ee3eb0ce85e957654828c4f2c77626ba2707
kabirasunny <sunnykabira5507@gmail.com>
2024-12-15 16:07:57 +0530

    i added a new api '/api/color for change color

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Example API endpoint
 app.get('/api/greet', (req, res) => {
-    res.json({ message: 'Hello from Node.js API sup'});
+    res.json({ message: 'Hello from Node.js API sup' });
 });
+
+app.get('/api/color', (req, res) => {
+    let color = ['red', 'green', 'blue', 'purple', 'orange', 'yellow', 'skyblue'];
+    let num = Math.floor(Math.random() * color.length);
+    res.json({ color: color[num] });
+})
 
 //Serve the HTML file for the root path
 app.get('/', (req, res) => {
